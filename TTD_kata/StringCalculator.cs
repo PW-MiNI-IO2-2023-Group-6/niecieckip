714998238c3d561fb5416699dac4e4a7b89b37a5
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-03-10 18:19:17 +0100

    bad change

--- TTD_kata/StringCalculator.cs
+++ TTD_kata/StringCalculator.cs
@@ -10,30 +10,6 @@ namespace TDD
     {
         public static int Calculate(string str)
         {
-            int result = 0;
-            char[] separators = { ',', '\n'};
-
-            if (String.IsNullOrEmpty(str))
-                return 0;
-
-            if (str.StartsWith("//"))
-            {
-                separators.Append(str[2]);
-                str = str[3..];
-            }
-
-            var split = str.Split(separators);
-
-            foreach (var s in split)
-            {
-                if (int.TryParse(s, out int number) && number <= 1000)
-                {
-                    if (number < 0)
-                        throw new ArgumentException();
-
-                    result += number;
-                }
-            }
 
             return result;
         }
